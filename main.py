d0b15308e78e2e76d1f92edd7e90debaaa94ad24
Gabriel <gabrielduarte1615@gmail.com>
2024-04-10 17:06:36 +0100

    Text File add and functinal

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 # API avançada 
-
 import flet as ft 
 
 def main(page):
-	pass
+    
+	page.title = "Chat - TP3"
+	page.window_width = 500
+	page.window_height = 600
+	
+	chat = ft.Column()
+	new_message = ft.TextField()
 
+	def send_click(e):
+		chat.controls.append(ft.Text(new_message.value))
+		new_message.value = ""
+		page.update()
+  
+	page.add(
+     	ft.Row(
+			[ft.Text(value=" \n Chat ", style="headlineMedium",color="White")],
+			alignment=ft.MainAxisAlignment.CENTER,
+			),
+			#CallCalculator(page)
+		chat, ft.Row(
+      		controls=[new_message, ft.ElevatedButton("Enviar", on_click=send_click)]
+        )
+	)
 
 if __name__ == "__main__":
-	ft.app(target=main)	
+	ft.app(target=main,assets_dir="assets",view=ft.AppView.WEB_BROWSER)
