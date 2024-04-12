b7b93c4a3b19fdc76a30201a8286acd32dddbffc
Gabriel <gabrielduarte1615@gmail.com>
2024-04-12 18:42:37 +0100

    Botão de delete e de edit adicionados, porem não funcionais

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ class Message():
         self.user_name = str(user_name)
         self.text = str(text)
         self.message_type = str(message_type)
+        self.id = id(self) # Adicionar um identificador a mensagem
 
 class ChatMessage(ft.Row):
     def __init__(self, message: Message):
-        super().__init__()
-        self.vertical_alignment="start"
+        super().__init__() # Vai buscar a class Row antes de iniciar a ChatMessage
+        self.message = message # Guarda a mensgaem 
+        self.vertical_alignment ="start"
         self.controls=[
-                ft.CircleAvatar(
+                ft.CircleAvatar( #Criar o avatar com o nome do usar e o formato
                     content=ft.Text(self.get_initials(message.user_name)),
                     color=ft.colors.WHITE,
                     bgcolor=self.get_avatar_color(message.user_name),
@@ -22,17 +24,31 @@ class ChatMessage(ft.Row):
                         ft.Text(message.text, selectable=True),
                     ],
                     tight=True,
-                    spacing=5,
+                    spacing=5, #Espaço entre o nome e o texto
                 ),
-            ]
+                ft.Row(
+                [
+                    ft.IconButton(
+                        icon=ft.icons.EDIT,
+                        tooltip="Edit message",
+                        on_click=self.edit_message
+                    ),
+                    ft.IconButton(
+                        icon=ft.icons.DELETE,
+                        tooltip="Delete message",
+                        on_click=self.delete_message(message.id)
+                    ),      
+                ],
+            ),
+        ]
 
     def get_initials(self, user_name):
         if user_name:
-            return user_name[:1].capitalize()
+            return user_name[:1].capitalize() #Coloca apenas a primeira letra como miuscula
         else:
-            return "Unknown"  # or any default value you prefer
+            return "Unknown"  
 
-    def get_avatar_color(self, user_name):
+    def get_avatar_color(self, user_name): #Define as cores do avatar
         colors_lookup = [
             ft.colors.AMBER,
             ft.colors.BLUE,
@@ -48,35 +64,45 @@ class ChatMessage(ft.Row):
             ft.colors.TEAL,
             ft.colors.YELLOW,
         ]
-        return colors_lookup[hash(user_name) % len(colors_lookup)]
-
+        return colors_lookup[hash(user_name) % len(colors_lookup)] #Ver esta linha 
+        
+    def edit_message(self):   
+        pass
+    
+    def delete_message(self,message_id):
+        pass
+    
 def main(page):
-    page.horizontal_alignment = "stretch"
     page.title = "Chat - TP3"  
 
     def join_chat_click(e):
+        #Se não colocarmos nenhum user name
         if not join_user_name.value:
             join_user_name.error_text = "Name cannot be blank!"
             join_user_name.update()
         else:
             page.session.set("user_name", join_user_name.value)
             page.dialog.open = False
+            
+            #Adiciona o prefix antes de escrever 
             new_message.prefix = ft.Text(f"{join_user_name.value}: ")
+            
+            #Ver a caor de texto---
             page.pubsub.send_all(Message(user_name=join_user_name.value, text=f"{join_user_name.value} has joined the chat.", message_type="login_message"))
             page.update()
 
     def send_message_click(e):
-        if new_message.value != "":
+        if new_message.value != "": # Se não existir nenhuma mensagem para enviar 
             page.pubsub.send_all(Message(page.session.get("user_name"), new_message.value, message_type="chat_message"))
-            new_message.value = ""
-            new_message.focus()
+            new_message.value = "" # Depois volta a vazio
+            new_message.focus() # Continuar com o campo selecionado
             page.update()
 
-    def on_message(message: Message):
+    def on_message(message: Message):  #Edita o meu message_type
         if message.message_type == "chat_message":
             m = ChatMessage(message)
         elif message.message_type == "login_message":
-            m = ft.Text(message.text, italic=True, color=ft.colors.BLACK45, size=12)
+            m = ft.Text(message.text, italic=True, color=ft.colors.WHITE54, size=12)
         chat.controls.append(m)
         page.update()
 
@@ -142,4 +168,8 @@ def main(page):
     )
 
 if __name__ == "__main__":
-	ft.app(port=8550, target=main, assets_dir="assets/",view=ft.WEB_BROWSER)
+	ft.app(target=main,
+            assets_dir="assets",
+            view=ft.WEB_BROWSER,
+            port=8550)
+ 
